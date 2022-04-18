1def6e0d1cc9c3b4fad66f9d749566713338b2e4
Enrique Núñez <nenrique0112@gmail.com>
2022-04-17 21:44:53 -0400

    Functionality implemented

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿List<string> usernames = new List<string>();
+List<string> comments = new List<string>();
+
+Console.WriteLine("sda");
+Console.ReadLine();
+
+using (var reader = new StreamReader(@"C:\Users\enriq\Desktop\comments-columns.csv"))
+{
+    while (!reader.EndOfStream)
+    {
+        string line = reader.ReadLine()!;
+        var values = line.Split(';');
+        usernames.Add(values[0]);
+        comments.Add(values[1]);
+    }
+    reader.Close();
+}
+
+print(usernames);
+Console.WriteLine("===============================");
+print(comments);
+
+Console.ReadLine();
+
+void print(List<string> list)
+{
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+}
