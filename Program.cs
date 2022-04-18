34e31b859cdbb81e92d74bd266ddd31cfdd15144
Enrique Núñez <nenrique0112@gmail.com>
2022-04-17 21:48:38 -0400

    Cleaned up some comments and tests

--- Program.cs
+++ Program.cs
@@ -1,24 +1,22 @@
 ﻿List<string> usernames = new List<string>();
 List<string> comments = new List<string>();
 
-Console.WriteLine("sda");
-Console.ReadLine();
+// Add your CSV's path
+var pathCSV = @"";
 
-using (var reader = new StreamReader(@"C:\Users\enriq\Desktop\comments-columns.csv"))
+using (var reader = new StreamReader(pathCSV))
 {
     while (!reader.EndOfStream)
     {
+        // Example made with two-column CSV
         string line = reader.ReadLine()!;
         var values = line.Split(';');
+        // Add more lists and columns as you need
         usernames.Add(values[0]);
         comments.Add(values[1]);
     }
     reader.Close();
 }
-
-print(usernames);
-Console.WriteLine("===============================");
-print(comments);
 
 Console.ReadLine();
 
